777111e8961e04939ec5c3a52581f2aba128df40
vue-study <18511283565@163.com>
2020-07-25 16:54:15 +0800

    学习到-表单输入绑定

---
+++ vue-test/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    {{ message }}
+    <transition name="fade">
+      <p v-if="show">now u can see me</p>
+    </transition>
+    <button v-on:click="show = !show">Toggle</button>
+  </div>
+</template>
+<script src="./js/vue.js" type="text/javascript" charset="utf-8"></script>
+<script>
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+    show: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
