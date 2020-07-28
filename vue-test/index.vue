e7035be2ba20352aee44ce3f7c1f67a2f8c9ad29
vue-study <18511283565@163.com>
2020-07-28 16:56:02 +0800

    Vue预习视频初步练习完成

--- vue-test/index.vue
+++ vue-test/index.vue
@@ -1,29 +1,9 @@
 <template>
-  <div id="app">
-    {{ message }}
-    <transition name="fade">
-      <p v-if="show">now u can see me</p>
-    </transition>
-    <button v-on:click="show = !show">Toggle</button>
-  </div>
+  <div></div>
 </template>
-<script src="./js/vue.js" type="text/javascript" charset="utf-8"></script>
+
 <script>
-var app = new Vue({
-  el: "#app",
-  data: {
-    message: "Hello Vue!",
-    show: true,
-  },
-});
+export default {};
 </script>
 
-<style lang="scss" scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-</style>
+<style lang="scss" scoped></style>
